780a363a935f98296366e308ffbdcb810cf7572e
Naotamai <24zb1093@student.gs.chiba-u.jp>
2025-07-11 14:41:40 +0900

    Update day05_homework.R

--- day05_homework.R
+++ day05_homework.R
@@ -1,16 +1,17 @@
 # DAY 5 HOMEWORK
 #
-# Name:
+# Name: NAO TAMAI
 
 # NOTE: In the text below, backticks ``
 # are used to indicate code. For example, `library(tidyverse)`
-
+install.packages("nycflights13")
 # Before doing anything else, load the packages you need for this assignment:
 # `tidyverse` and `nycflights13`
-
+library(tidyverse)
+library(nycflights13)
 # Q1. The `nycflights13` package includes a dataframe called `planes` that
 # contains data for each airplane such as manufacturer, model, etc.
-#
+answer_1 <- flights %>% left_join(planes, by = "tailnum")
 # Join the `planes` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_1
 #
@@ -19,25 +20,28 @@
 # Q2. Use answer_1 to count the number of flights made by each manufacturer,
 # then sort the results so the manufacturer with the most flights is at the top.
 # Save the result as answer_2
-#
+answer_2 <- answer_1 %>% count(manufacturer, sort = TRUE)
 # HINT: use `count()`
 
 # Q3. Does every flight in the `flights` dataframe have a corresponding
 # airplane in `planes`?
-#
+answer_3 <- !any(is.na(answer_1$manufacturer))
 # Save your answer as a logical vector (either TRUE or FALSE) to answer_3
 
 # Q4. The `nycflights13` package includes a dataframe called `airlines` that
 # includes the name of each airline and its corresponding carrier code.
 # For example, the carrier code for American Airlines Inc. is AA.
-#
+answer_4 <- flights %>% left_join(airlines, by = "carrier")
+
+
+
 # Join the `airlines` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_4
 
 # Q5. Use answer_4 to find the average arrival delay for each airline.
 # Exclude missing values from your calculation.
 # Save the result as answer_5
-#
+answer_5 <- answer_4 %>% group_by(name) %>% summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>% arrange(desc(avg_arr_delay))
 # HINT: remember that you can use `group_by()` and `summarize()` together to
 # calculate summary statistics for each group in the data.
 #
